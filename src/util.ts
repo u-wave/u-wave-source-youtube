4bd203d970232b9a45a26ad1fd3d805f0a412a91
Renée Kooi <renee@kooi.me>
2022-05-18 10:09:53 +0200

    Add sourceTitle / sourceArtist properties

--- src/util.ts
+++ src/util.ts
@@ -62,6 +62,8 @@ export interface UwMedia {
   duration: number;
   thumbnail: string;
   sourceData: {
+    sourceTitle: string;
+    sourceArtist: string;
     embedWidth: number | null,
     embedHeight: number | null,
     blockedIn: string[],
@@ -92,6 +94,9 @@ export function normalizeMedia(video: VideoResource): UwMedia {
     duration,
     thumbnail: getBestThumbnail(video.snippet.thumbnails),
     sourceData: {
+      // Shown in search results.
+      sourceTitle: video.snippet.title,
+      sourceArtist: video.snippet.channelTitle,
       // Can be used by clients to determine the aspect ratio.
       embedWidth: video.player?.embedWidth ?? null,
       embedHeight: video.player?.embedHeight ?? null,
